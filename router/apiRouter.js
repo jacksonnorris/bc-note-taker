f6b2048d1524739dacd2cf6c632fdcbff5d47c5e
jacksonnorris <jacksonnorr@gmail.com>
2022-03-25 15:07:14 -0400

    can now add notes

--- router/apiRouter.js
+++ router/apiRouter.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const path = require('path');
 const notes = require('../db/db.json');
 const fs = require('fs');
+const { text } = require('express');
 
 router.get('/notes', (req, res) => {
     fs.readFile('./db/db.json', 'utf-8', function(err, data) {
@@ -11,10 +12,23 @@ router.get('/notes', (req, res) => {
 });
 
 router.post('/notes', (req, res) => {
+//    console.log(req.body);
+   const note = { 
+       title: req.body.title,
+       text: req.body.text
+    }
+    var info = fs.readFileSync('./db/db.json', 'utf-8');
+    // console.log(info);
+
+    var currentData = JSON.parse(info);
+    currentData.push(note);
+    console.log(currentData);
+    fs.writeFileSync('./db/db.json', JSON.stringify(currentData));
+    
     // fs.writeFile()
 });
 
-router.get('../db/db.json', (req, res) => {
+router.get('./db/db.json', (req, res) => {
     console.info(res.status(200).json(notes));
 });
 
