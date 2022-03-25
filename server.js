d732b7a43e1d9d982c75d7d24947f7ae014ecd3b
jacksonnorris <jacksonnorr@gmail.com>
2022-03-25 13:33:42 -0400

    can change to notes page

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({
     extended:true
 }));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 app.get('/notes', (req, res) => {
