5517e7029731508fc05bdf44958a676c47465449
jacksonnorris <jacksonnorr@gmail.com>
2022-03-25 21:06:42 -0400

    added README info, deleted commented out code and console.logs

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# bc-note-taker
+# Note Taker
+
+[Live link to application](https://ancient-sea-45779.herokuapp.com/)
+
+## Features
+
+```
+    This application allows users to take notes and store them in a json database
+    Users can save a note after entering a title and description and clicking on the save icon
+    Users can access saved notes by clicking on the title on the left-hand side of the screen
+    Users can delete any notes by clicking on the trash can icon
+```
--- router/apiRouter.js
+++ router/apiRouter.js
@@ -9,45 +9,30 @@ router.get('/notes', (req, res) => {
     fs.readFile('./db/db.json', 'utf-8', function(err, data) {
         res.json(JSON.parse(data));
     })
-    // res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
 router.post('/notes', (req, res) => {
-//    console.log(req.body);
    const note = { 
        title: req.body.title,
        text: req.body.text,
        id: uuidv4()
     }
     var info = fs.readFileSync('./db/db.json', 'utf-8');
-    // console.log(info);
 
     var currentData = JSON.parse(info);
     currentData.push(note);
     console.log(currentData);
     fs.writeFileSync('./db/db.json', JSON.stringify(currentData));
     res.json(currentData);
-    // fs.writeFile()
 });
 
 router.delete('/notes/:id', (req, res) => {
     var noteId = req.params.id;
-    console.log(noteId);
     var info = fs.readFileSync('./db/db.json', 'utf-8');
     var currentData = JSON.parse(info);
-    // for (var note in currentData) {
-    //     if (currentData[note].id === noteId) {
-    //         console.log('found')
-    //     }
-    // }
     let newData = currentData.filter(note => note.id !== noteId);
-    console.log(newData);
     fs.writeFileSync('./db/db.json', JSON.stringify(newData));
     res.json(newData);
-})
-
-router.get('./db/db.json', (req, res) => {
-    console.info(res.status(200).json(notes));
 });
 
 
